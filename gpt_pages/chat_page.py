c2610ce37786af318a367d30334f215afee9fc4a
chygai232 <chygai232@gmail.com>
2024-08-07 10:27:10 +0900

    대화형 채팅기능 추가

--- gpt_pages/chat_page.py
+++ gpt_pages/chat_page.py
@@ -1,3 +1,93 @@
 import streamlit as st
 
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.prompts import MessagesPlaceholder
+from langchain_openai import ChatOpenAI
+from langchain_core.chat_history import (BaseChatMessageHistory, InMemoryChatMessageHistory)
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_core.messages import HumanMessage
+
+# 채팅 히스토리 초기화
+if "store" not in st.session_state:
+    st.session_state.store = {}
+
+if "messages" not in st.session_state:
+    st.session_state["messages"] = []
+
 st.title("My ChatGPT")
+
+# 세션 아이디(session_id)를 받아서 메세지 히스토리 오브젝트를 반환
+def get_session_history(session_id: str) -> BaseChatMessageHistory:
+    if session_id not in st.session_state.store:
+        st.session_state.store[session_id] = InMemoryChatMessageHistory()
+    return st.session_state.store[session_id]
+
+# 채팅 체인 생성
+def create_chat_chain():
+    # 프롬프트
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", "You are a helpful assistant. Answer all questions to the best of your ability."),
+            MessagesPlaceholder(variable_name="messages"),
+        ]
+    )
+    
+    # GPT 모델
+    llm = ChatOpenAI(
+        model="gpt-4o-mini",
+        temperature=0,
+        max_tokens=None,
+        timeout=None,
+        max_retries=2,
+        api_key=st.session_state.openai_api_key,
+    )
+
+    # 체인 생성
+    chat_chain = prompt | llm
+
+    # 메시지 히스토리 조회 단계 추가
+    chat_chain_with_history = RunnableWithMessageHistory(
+        chat_chain, 
+        get_session_history,
+    )
+
+    return chat_chain_with_history
+
+# 세션정보
+config = {"configurable": {"session_id": "abc2"}}
+
+# AI 답변 생성
+def generate_response(human_message):
+    chain = create_chat_chain()    # 체인 생성
+
+    # 답변 생성
+    # response = chain.invoke(
+    #     [HumanMessage(content=human_message)],
+    #     config=config
+    # )
+    
+    return chain.stream(
+        [HumanMessage(content=human_message)],
+        config=config
+    )
+
+# 앱 재실행시 채팅 메시지 표시
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# 메시지 컨테이너
+if prompt := st.chat_input("Please enter your message"):
+    # 채팅 메시지 컨테이너에 사용자 메시지 표시
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    # 채팅 히스토리에 사용자 메시지 추가        
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    # 채팅 메시지 컨테이너에 GPT 메시지 표시
+    with st.chat_message("assistant"):
+        response = st.write_stream(generate_response(prompt))   # AI 답변 생성
+
+    # 채팅 히스토리에 GPT 메시지 추가
+    st.session_state.messages.append({"role": "assistant", "content": response})
--- setting_pages/my_page.py
+++ setting_pages/my_page.py
@@ -1,3 +1,20 @@
 import streamlit as st
 
 st.title("My Page")
+
+# OPENAI API KEY 입력 필드
+openai_api_key = st.text_input("OPENAI API KEY")
+
+def set_openai_api_key(api_key):
+    st.session_state.openai_api_key = api_key
+
+st.button('save', key="openai_save_btn", on_click=set_openai_api_key, args=(openai_api_key, ), type="primary")
+
+# LANGSMITH API KEY 입력 필드
+
+def set_langsmith_api_key(api_key):
+    st.session_state.langsmith_api_key = api_key
+
+langsmith_api_key = st.text_input("LANGSMITH API KEY")
+
+st.button('save', key="lngsmh_save_btn", on_click=set_langsmith_api_key, args=(langsmith_api_key, ), type="primary")
